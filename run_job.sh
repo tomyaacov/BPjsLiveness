be043e91c859078f1e3428ca51b7cfba3af930ac
tomyaacov <tomyaacov1210@gmail.com>
2023-01-15 12:54:14 +0200

    first commit

--- run_job.sh
+++ run_job.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 ### sbatch config parameters must start with #SBATCH and must precede any other command. to ignore just add another # - like so ##SBATCH
 #SBATCH --partition main ### specify partition name where to run a job
-##SBATCH --time 7-00:00:00 ### limit the time of job running. Format: D-H:MM:SS
+#SBATCH --time 7-00:00:00 ### limit the time of job running. Format: D-H:MM:SS
 #SBATCH --job-name run_job ### name of the job. replace my_job with your desired job name
-## no need to redirect out and err to different files. By default they are redirected to slurm-%j.out
-##SBATCH --output run_job.out ### output log for running job - %J is the job number variable
-##SBATCH --error run_job.err ### output log for running job - %J is the job number variable
+#SBATCH --output run_job.out ### output log for running job - %J is the job number variable
 #SBATCH --mail-user=tomya@post.bgu.ac.il ### users email for sending job status notifications ñ replace with yours
 #SBATCH --mail-type=BEGIN,END,FAIL ### conditions when to send the email. ALL,BEGIN,END,FAIL, REQUEU, NONE
 #SBATCH --mem=64G ### total amount of RAM // 500
