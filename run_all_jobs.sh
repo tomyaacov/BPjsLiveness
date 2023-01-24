f709483601a37184bcc3867d5de19445b08eb3e3
tomyaacov <tomyaacov1210@gmail.com>
2023-01-24 16:47:07 +0200

    adding more maps

--- run_all_jobs.sh
+++ run_all_jobs.sh
@@ -2,8 +2,8 @@
 ### sbatch config parameters must start with #SBATCH and must precede any other command. to ignore just add another # - like so ##SBATCH
 #SBATCH --partition main ### specify partition name where to run a job
 #SBATCH --time 7-00:00:00 ### limit the time of job running. Format: D-H:MM:SS
-#SBATCH --job-name run_all_jobs ### name of the job. replace my_job with your desired job name
-#SBATCH --output run_all_jobs.out ### output log for running job - %J is the job number variable
+#SBATCH --job-name run_all_jobs2 ### name of the job. replace my_job with your desired job name
+#SBATCH --output run_all_jobs2.out ### output log for running job - %J is the job number variable
 #SBATCH --mail-user=tomya@post.bgu.ac.il ### users email for sending job status notifications ñ replace with yours
 #SBATCH --mail-type=BEGIN,END,FAIL ### conditions when to send the email. ALL,BEGIN,END,FAIL, REQUEU, NONE
 #SBATCH --mem=128G ### total amount of RAM // 500
@@ -18,7 +18,8 @@ export MAVEN_OPTS="-Xms128g -Xmx128g"
 mvn compile > /dev/null 2>&1
 #files=("map_8_7_2" "map_8_8_2" "map_8_9_2" "map_9_7_2_" "map_9_8_2" "map_9_9_2" "map_10_7_2" "map_10_8_2" "map_10_9_2" "map_10_10_2" "map_11_8_2" "map_11_9_2")
 #files=("map_6_6_3" "map_6_7_3" "map_7_6_3" "map_7_7_3" )
-files=("map_7_7_3" "map_6_7_3" "map_11_9_2" "map_7_6_3" "map_10_9_2" "map_6_6_3" "map_9_7_2_" "map_8_9_2" "map_8_7_2" "map_11_8_2" "map_9_8_2" "map_9_9_2" )
+#files=("map_7_7_3" "map_6_7_3" "map_11_9_2" "map_7_6_3" "map_10_9_2" "map_6_6_3" "map_9_7_2_" "map_8_9_2" "map_8_7_2" "map_11_8_2" "map_9_8_2" "map_9_9_2" )
+files=("map_7_7_3" "map_6_7_3" "map_11_9_2" "map_10_9_2" )
 for file in "${files[@]}"; do
   echo "$file"
   mvn exec:java -D"exec.args"="$file"
