d9472dbd5c3f6cc3a8af979eebf954683d153d13
tomyaacov <tomyaacov1210@gmail.com>
2023-01-24 15:08:14 +0200

    adding more maps

--- run_all_jobs.sh
+++ run_all_jobs.sh
@@ -6,7 +6,7 @@
 #SBATCH --output run_all_jobs.out ### output log for running job - %J is the job number variable
 #SBATCH --mail-user=tomya@post.bgu.ac.il ### users email for sending job status notifications ñ replace with yours
 #SBATCH --mail-type=BEGIN,END,FAIL ### conditions when to send the email. ALL,BEGIN,END,FAIL, REQUEU, NONE
-#SBATCH --mem=250G ### total amount of RAM // 500
+#SBATCH --mem=128G ### total amount of RAM // 500
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32 ##. // max 128
 
@@ -14,7 +14,7 @@
 module load anaconda ### load anaconda module
 source activate bpjs_liveness ### activating Conda environment. Environment must be configured before running the job
 cd ~/repos/BPjsLiveness/ || exit
-export MAVEN_OPTS="-Xms250g -Xmx250g"
+export MAVEN_OPTS="-Xms128g -Xmx128g"
 mvn compile > /dev/null 2>&1
 #files=("map_8_7_2" "map_8_8_2" "map_8_9_2" "map_9_7_2_" "map_9_8_2" "map_9_9_2" "map_10_7_2" "map_10_8_2" "map_10_9_2" "map_10_10_2" "map_11_8_2" "map_11_9_2")
 #files=("map_6_6_3" "map_6_7_3" "map_7_6_3" "map_7_7_3" )
