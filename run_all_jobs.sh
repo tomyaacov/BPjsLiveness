b991669c7e5f1ebbda0941df92314751ef1a58e0
tomyaacov <tomyaacov1210@gmail.com>
2023-01-24 12:37:40 +0200

    adding more maps

--- run_all_jobs.sh
+++ run_all_jobs.sh
@@ -16,7 +16,8 @@ source activate bpjs_liveness ### activating Conda environment. Environment must
 cd ~/repos/BPjsLiveness/ || exit
 export MAVEN_OPTS="-Xms250g -Xmx250g"
 mvn compile > /dev/null 2>&1
-files=("map_8_7_2" "map_8_8_2" "map_8_9_2" "map_9_7_2_" "map_9_8_2" "map_9_9_2" "map_10_7_2" "map_10_8_2" "map_10_9_2" "map_10_10_2" "map_11_8_2" "map_11_9_2")
+#files=("map_8_7_2" "map_8_8_2" "map_8_9_2" "map_9_7_2_" "map_9_8_2" "map_9_9_2" "map_10_7_2" "map_10_8_2" "map_10_9_2" "map_10_10_2" "map_11_8_2" "map_11_9_2")
+files=("map_6_6_3" "map_6_7_3" "map_7_6_3" "map_7_7_3" )
 for file in "${files[@]}"; do
   echo "$file"
   mvn exec:java -D"exec.args"="$file"
