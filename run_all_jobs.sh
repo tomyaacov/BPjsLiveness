d58a55aec88268aa17f87654b141413d1ffe597b
tomyaacov <tomyaacov1210@gmail.com>
2023-01-25 09:01:49 +0200

    adding more maps

--- run_all_jobs.sh
+++ run_all_jobs.sh
@@ -19,7 +19,7 @@ mvn compile > /dev/null 2>&1
 #files=("map_8_7_2" "map_8_8_2" "map_8_9_2" "map_9_7_2_" "map_9_8_2" "map_9_9_2" "map_10_7_2" "map_10_8_2" "map_10_9_2" "map_10_10_2" "map_11_8_2" "map_11_9_2")
 #files=("map_6_6_3" "map_6_7_3" "map_7_6_3" "map_7_7_3" )
 #files=("map_7_7_3" "map_6_7_3" "map_11_9_2" "map_7_6_3" "map_10_9_2" "map_6_6_3" "map_9_7_2_" "map_8_9_2" "map_8_7_2" "map_11_8_2" "map_9_8_2" "map_9_9_2" )
-files=("map_7_7_3" "map_6_7_3" "map_11_9_2" "map_10_9_2" )
+files=("map_9_8_2" "map_11_9_2" "map_10_9_2" "map_9_9_2" )
 for file in "${files[@]}"; do
   echo "$file"
   mvn exec:java -D"exec.args"="$file"
