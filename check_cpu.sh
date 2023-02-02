515395034df7e27610c147c82c0920bda9fc3d9c
tomyaacov <tomyaacov1210@gmail.com>
2023-02-02 13:13:42 +0200

    adding more maps

---
+++ check_cpu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+### sbatch config parameters must start with #SBATCH and must precede any other command. to ignore just add another # - like so ##SBATCH
+#SBATCH --partition main ### specify partition name where to run a job
+#SBATCH --time 7-00:00:00 ### limit the time of job running. Format: D-H:MM:SS
+#SBATCH --job-name check_cpu ### name of the job. replace my_job with your desired job name
+#SBATCH --output check_cpu.out ### output log for running job - %J is the job number variable
+#SBATCH --mail-user=tomya@post.bgu.ac.il ### users email for sending job status notifications ñ replace with yours
+#SBATCH --mail-type=BEGIN,END,FAIL ### conditions when to send the email. ALL,BEGIN,END,FAIL, REQUEU, NONE
+#SBATCH --mem=1G ### total amount of RAM // 500
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32 ##. // max 128
+
+### Start you code below ####
+cat /proc/cpuinfo
