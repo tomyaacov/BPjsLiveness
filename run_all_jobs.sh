381a5aeec488889b8fa70f650c171529d7746c32
tomyaacov <tomyaacov1210@gmail.com>
2023-02-02 08:22:40 +0200

    adding more maps

--- run_all_jobs.sh
+++ run_all_jobs.sh
@@ -2,11 +2,11 @@
 ### sbatch config parameters must start with #SBATCH and must precede any other command. to ignore just add another # - like so ##SBATCH
 #SBATCH --partition main ### specify partition name where to run a job
 #SBATCH --time 7-00:00:00 ### limit the time of job running. Format: D-H:MM:SS
-#SBATCH --job-name run_all_jobs2 ### name of the job. replace my_job with your desired job name
-#SBATCH --output run_all_jobs2.out ### output log for running job - %J is the job number variable
+#SBATCH --job-name run_all_jobs ### name of the job. replace my_job with your desired job name
+#SBATCH --output run_all_jobs.out ### output log for running job - %J is the job number variable
 #SBATCH --mail-user=tomya@post.bgu.ac.il ### users email for sending job status notifications ñ replace with yours
 #SBATCH --mail-type=BEGIN,END,FAIL ### conditions when to send the email. ALL,BEGIN,END,FAIL, REQUEU, NONE
-#SBATCH --mem=128G ### total amount of RAM // 500
+#SBATCH --mem=250G ### total amount of RAM // 500
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32 ##. // max 128
 
@@ -14,12 +14,13 @@
 module load anaconda ### load anaconda module
 source activate bpjs_liveness ### activating Conda environment. Environment must be configured before running the job
 cd ~/repos/BPjsLiveness/ || exit
-export MAVEN_OPTS="-Xms128g -Xmx128g"
+export MAVEN_OPTS="-Xms250g -Xmx250g"
 mvn compile > /dev/null 2>&1
 #files=("map_8_7_2" "map_8_8_2" "map_8_9_2" "map_9_7_2_" "map_9_8_2" "map_9_9_2" "map_10_7_2" "map_10_8_2" "map_10_9_2" "map_10_10_2" "map_11_8_2" "map_11_9_2")
 #files=("map_6_6_3" "map_6_7_3" "map_7_6_3" "map_7_7_3" )
 #files=("map_7_7_3" "map_6_7_3" "map_11_9_2" "map_7_6_3" "map_10_9_2" "map_6_6_3" "map_9_7_2_" "map_8_9_2" "map_8_7_2" "map_11_8_2" "map_9_8_2" "map_9_9_2" )
-files=( "map_10_9_2")
+#files=( "map_10_9_2")
+files=("map_6_8_3" "map_12_11_1" "map_13_12_1" "map_9_10_2" "map_7_7_2" "map_7_7_3" "map_11_9_2" "map_6_6_3" "map_6_7_3" "map_8_8_2" "map_8_2" "map_10_9_2" "map_9_7_2_" "map_9_7_2" "map_10_7_2" "map_8_9_2" "map_8_7_2" "map_9_8_2" "map_9_9_2" "map_11_8_2")
 for file in "${files[@]}"; do
   echo "$file"
   mvn exec:java -D"exec.args"="$file"
